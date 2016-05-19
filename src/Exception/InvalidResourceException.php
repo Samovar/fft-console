4240a261493a62bc4ac3f99f7548c0deca7f640f
samovar <prototype.denis@gmail.com>
2016-05-19 21:36:31 +0300

    InvalidResourceException

---
+++ src/Exception/InvalidResourceException.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of the Samovar/FFTConsole package.
+ *
+ * (c) Denis Buzdygar <prototype.denis@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Samovar\FFTConsole\Exception;
+
+/**
+ * @author Denis Buzdygar <prototype.denis@gmail.com>
+ */
+class InvalidResourceException extends \InvalidArgumentException
+{
+}
